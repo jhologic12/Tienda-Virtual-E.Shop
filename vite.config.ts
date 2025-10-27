5741db34c17f0ffa6f8f7a018279dd49035985ec
Jhon Ospino <jaofdev@gmail.com>
2025-10-27 08:16:06 -0500

    se configurar los archivos package-lock.json para compatibilidad

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 5173,
-    allowedHosts: ['tienda-virtual-e-shop.onrender.com'], // <--- tu dominio Render permitido
+    allowedHosts: ['tienda-virtual-e-shop.onrender.com'],
   },
   build: {
     outDir: 'dist',
